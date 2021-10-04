3b6d5bf144202cbba10c4caeed1a96b3ff3ddc26
Raunak Gupta <emperordevil.gupta@gmail.com>
2021-10-04 22:47:25 +0530

    Create in.php

---
+++ src/i18n/in.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'isoCode' => 'IN',
+    "tld" => [".in"], // country code top-level domain
+    "cca2" => "IN", // code ISO 3166-1 alpha-
+    "ccn3" => "356", // code ISO 3166-1 numeric
+    "cca3" => "IND", // code ISO 3166-1 alpha-3
+    "cioc" => "IND",
+    'common' => _('India'), // common name in english
+    'official' => _('Republic of India'), // official name in english
+    'native' => _(''), //  native name
+    'dialing_code' => _(''),
+    'continent' => 'AS',
+    'subregion' => _('Southern Asia'),
+    'capital' => [_('New Delhi')],
+    'currencies' => ['INR'],
+    'languages' => ['eng', 'hin', 'tam'],
+//    'languages' => [
+//        '' => [
+//            'name' => 'Arabic',
+//            'native' => 'العربية',
+//            'rtl' => true
+//        ],
+//    ],
+    'emoji' => _(''),
+    'emojiU' => _(''),
+    'latitude' => '20.00000000',
+    'longitude' => '77.00000000',
+    'timezones' => [
+        [
+            'zoneName' => 'Asia\/Kolkata',
+            'gmtOffset' => 19800,
+            'gmtOffsetName' => 'UTC+05:30',
+            'abbreviation' => 'IST',
+            'tzName' => 'Indian Standard Time'
+        ]
+    ],
+    'landlocked' => false,
+    'borders' => ['PK, BD', 'SL'],
+    'area' => _(3287590),
+    'address_format' => _(''),
+    'ethnicity' => _('Indian'),
+    '' => _(''),
+    '' => _(''),
+];
